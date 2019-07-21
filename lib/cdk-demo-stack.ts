5cb9f0c354dd50a9b3286e0460d9ee900f38e33e
Alastair Waddell <ali@isp20.com>
2019-07-21 23:24:28 +1000

    Squashed commit of the following:
    commit d6dc352deb03409dc3633ca0ad3ee4302d022955
    Author: Alastair Waddell <ali@isp20.com>
    Date:   Sun Jul 21 23:18:32 2019 +1000
    
        add_vpn change private subnet to type isolated
    
        A workaround to remove the NAT GW and associated default route
    
    commit 4781454f8795b28293a45f63f5e91e56ec953dd9
    Author: Alastair Waddell <ali@isp20.com>
    Date:   Sun Jul 21 21:13:24 2019 +1000
    
        add_vpc set nat gateways to zero
    
    commit d1d724f14c0daa9b0af7d473d95b5d4933eb4f46
    Author: Alastair Waddell <ali@isp20.com>
    Date:   Sun Jul 21 21:12:51 2019 +1000
    
        add_vpc add the vpc with props for cidr and subnet config

--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -1,9 +1,30 @@
 import cdk = require('@aws-cdk/core');
+import ec2 = require('@aws-cdk/aws-ec2');
 
 export class CdkDemoStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'TheVPC', {
+      cidr: '172.16.0.0/21',
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'Ingress',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: 'Application',
+          subnetType: ec2.SubnetType.ISOLATED,
+        },
+        {
+          cidrMask: 28,
+          name: 'Database',
+          subnetType: ec2.SubnetType.ISOLATED,
+        }
+      ]
+    });
   }
 }
