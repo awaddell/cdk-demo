5888e8cda38001d2ca128b4db9aaea84039fa471
Alastair Waddell <ali@isp20.com>
2019-07-21 23:41:41 +1000

    add asg and elb boilerplate

--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -1,5 +1,7 @@
 import cdk = require('@aws-cdk/core');
 import ec2 = require('@aws-cdk/aws-ec2');
+import autoscaling = require('@aws-cdk/aws-autoscaling');
+import elbv2 = require('@aws-cdk/aws-elasticloadbalancingv2');
 
 export class CdkDemoStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -25,6 +27,32 @@ export class CdkDemoStack extends cdk.Stack {
           subnetType: ec2.SubnetType.ISOLATED,
         }
       ]
+    });
+    
+    const asg = new autoscaling.AutoScalingGroup(this, 'ASG', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: new ec2.AmazonLinuxImage(),
+    });
+
+    const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
+      vpc,
+      internetFacing: true
+    });
+
+    const listener = lb.addListener('Listener', {
+      port: 80,
+    });
+
+    listener.addTargets('Target', {
+      port: 80,
+      targets: [asg]
+    });
+
+    listener.connections.allowDefaultPortFromAnyIpv4('Open to the world');
+
+    asg.scaleOnRequestCount('AModestLoad', {
+      targetRequestsPerSecond: 1
     });
   }
 }
