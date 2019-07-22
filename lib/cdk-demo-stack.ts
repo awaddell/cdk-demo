adf7d673d6c9d71ee214527fc9cd42b41258da29
Alastair Waddell <ali@isp20.com>
2019-07-22 13:52:38 +1000

    add custom userdata
    So we can change the web content later

--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -39,6 +39,34 @@ export class CdkDemoStack extends cdk.Stack {
       }),
     });
 
+    const userData = `
+    Content-Type: multipart/mixed; boundary="//"
+    MIME-Version: 1.0
+    
+    --//
+    Content-Type: text/cloud-config; charset="us-ascii"
+    MIME-Version: 1.0
+    Content-Transfer-Encoding: 7bit
+    Content-Disposition: attachment; filename="cloud-config.txt"
+    
+    #cloud-config
+    cloud_final_modules:
+    - [scripts-user, always]
+    
+    --//
+    Content-Type: text/x-shellscript; charset="us-ascii"
+    MIME-Version: 1.0
+    Content-Transfer-Encoding: 7bit
+    Content-Disposition: attachment; filename="userdata.txt"
+    
+    #!/bin/bash
+    /bin/echo "Hello World" >> /tmp/testfile.txt
+
+    --//
+    `
+
+    asg.addUserData(userData);
+
     const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
       vpc,
       internetFacing: true
